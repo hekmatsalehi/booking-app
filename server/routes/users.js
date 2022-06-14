1382869d443e8ba0e66d42299b54bb41486353c2
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2022-06-14 01:52:52 -0700

    Created routes for user

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import { deleteUser, getAllUsers, getSingleUser, updateUser } from '../controllers/user.js';
 
 const router = express.Router()
 
+// Update a user
+router.put("/:id", updateUser);
+
+// Delete a user
+router.delete("/:id", deleteUser);
+
+// Get single user
+router.get("/:id", getSingleUser);
+
+// Get all users
+router.get("/", getAllUsers);
 
 export default router;
