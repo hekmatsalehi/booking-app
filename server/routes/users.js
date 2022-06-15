6932f96205cfb524bba4d2765017e378c935999c
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2022-06-14 17:55:29 -0700

    Added verification for users

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,18 @@
 import express from 'express';
 import { deleteUser, getAllUsers, getSingleUser, updateUser } from '../controllers/user.js';
+import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifies.js';
 
 const router = express.Router()
+
+router.get("/checkauth", verifyToken, (req, res, next) => {
+   res.send('Hello your are logged in')  
+});
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+   res.send('Logged in, Can Delete your account')  
+});
+router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+   res.send('Logged in as Admin, Can Delete any account')  
+});
 
 // Update a user
 router.put("/:id", updateUser);
---
+++ server/utils/verifies.js
@@ -0,0 +1,39 @@
+import jwt from 'jsonwebtoken';
+import { createError } from './error.js';
+
+export const verifyToken = ( req, res, next ) => {
+    //get token from cookies
+    const token = req.cookies.access_token;
+    if(!token) {
+        return next(createError(401, 'You are not authenticated!'))
+    }
+    // verify token
+    jwt.verify(token, process.env.JWTSECRET, (err, userInfo) => {
+        if(err) {
+            return next(createError(403, 'Not a valid token!'))
+        }
+        req.user = userInfo;// in req obj add the userInfo(id & isAdmin)
+        next()
+    })
+}
+
+export const verifyUser = (req, res, next) => {
+    verifyToken(req, res, () => {
+        // check that the user is the same who is loggedIn
+        if(req.user.id === req.params.id || req.user.isAdmin) {
+            next()
+        } else {
+            return next(createError(403, 'You are not authorized!'))
+        }
+    })
+}
+export const verifyAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+        // check that the user is admin or not
+        if(req.user.isAdmin) {
+            next()
+        } else {
+            return next(createError(403, 'You are not authorized!'))
+        }
+    })
+}
