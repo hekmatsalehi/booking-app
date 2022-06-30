e38b84e6fae44e1ddd5b8a02f47429b88bba4706
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2022-06-30 02:00:31 -0700

    Worked on login feature

--- client/src/contex/AuthContex.js
+++ client/src/contex/AuthContex.js
@@ -1,9 +1,9 @@
 import { createContext, useEffect, useReducer } from "react";
 
 const INITIAL_STATE = {
-    user: JSON.parse(localStorage.getItem("user")) || null,
-    loading: false,
-    error: null
+  user: JSON.parse(localStorage.getItem("user")) || null,
+  loading: false,
+  error: null,
 };
 
 export const AuthContex = createContext(INITIAL_STATE);
@@ -11,44 +11,42 @@ export const AuthContex = createContext(INITIAL_STATE);
 const AuthReducer = (state, action) => {
   switch (action.type) {
     case "LOGIN_START":
-        return {
-            user: null,
-            loading: true,
-            error: null
-        };
+      return {
+        user: null,
+        loading: true,
+        error: null,
+      };
     case "LOGIN_SUCCESS":
-        return {
-            user: action.payload,
-            loading: false,
-            error: null
-        };
+      return {
+        user: action.payload,
+        loading: false,
+        error: null,
+      };
     case "LOGIN_FAIL":
-        return {
-            user: null,
-            loading: false,
-            error: action.payload
-        };
-    case "LOGOUT": 
-        return {
-            user: null,
-            loading: false,
-            error: null
-        };
+      return {
+        user: null,
+        loading: false,
+        error: action.payload,
+      };
+    case "LOGOUT":
+      return {
+        user: null,
+        loading: false,
+        error: null,
+      };
     default:
-        return state;
-
+      return state;
   }
 };
 
 export const AuthContexProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
 
-
   // Save the loggedIn user to LocalStorage to not logout with page refresh
   useEffect(() => {
-   localStorage.setItem("user", JSON.stringify(state.user)) 
-  },[state.user]);
-  
+    localStorage.setItem("user", JSON.stringify(state.user));
+  }, [state.user]);
+
   return (
     <AuthContex.Provider
       value={{
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,16 +1,59 @@
-import './login.css';
-
+import axios from "axios";
+import { useContext, useState } from "react";
+import { AuthContex } from "../../contex/AuthContex";
+import "./login.css";
 
 const Login = () => {
-    return (
-        <div className='login'>
-            <div className="login-container">
-                <input type="text" placeholder='username' id="username" className="login-input"/>
-                <input type="text" placeholder='email' id="email" className="login-input"/>
-                <button className="login-btn">Login</button>
-            </div>
-        </div>
-    )
-}
+  const [credentials, setCredentials] = useState({
+    username: undefined,
+    password: undefined,
+  });
+
+  const { user, loading, error, dispatch } = useContext(AuthContex);
+
+  const handleChange = (e) => {
+    setCredentials( (prev) => ({
+        ...prev, [e.target.id]: e.target.value
+    }))
+  }
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    dispatch({type: "LOGIN_START"})
+    try {
+        const response = await axios.post("/auth/login", credentials);
+        dispatch({type: "LOGIN_SUCCESS", payload: response.data});
+    } catch (err) {
+        dispatch({type: "LOGIN_FAIL", payload: err.response.data})
+    }
+  }
+
+  console.log(user)
+  return (
+    <div className="login">
+      <div className="login-container">
+        <h2>Log in to your account</h2>
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="login-input"
+          onChange={handleChange}
+          
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="login-input"
+          onChange={handleChange}
+          
+        />
+        <button className="login-btn" onClick={handleLogin}>Login</button>
+        <div className="login-error">{error && <span>{error.message}</span>}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
